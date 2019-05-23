aab865ac1c00cd486f7a6c00a7c4ad0b9dc3e58b
liyatang <liyatang@qq.com>
2019-05-23 17:59:08 +0800

    优化

--- trello.user.js
+++ trello.user.js
@@ -1,50 +1,58 @@
-function showMember() {
-	if (!window.location.href.includes('trello.com')) {
-		return
-	}
+function showMember () {
+  if (!window.location.href.includes('trello.com')) {
+    return
+  }
 
-	const url = `https://trello.com/1/Boards/${location.pathname.split('/')[2]}?lists=open&list_fields=name%2Cclosed%2CidBoard%2Cpos%2Csubscribed%2Climits%2CcreationMethod%2CsoftLimit&cards=visible&card_attachments=cover&card_stickers=true&card_fields=badges%2Cclosed%2CdateLastActivity%2Cdesc%2CdescData%2Cdue%2CdueComplete%2CdueReminder%2CidAttachmentCover%2CidList%2CidBoard%2CidMembers%2CidShort%2CidLabels%2Climits%2Cname%2Cpos%2CshortUrl%2CshortLink%2Csubscribed%2Curl%2ClocationName%2Caddress%2Ccoordinates%2Clabels&card_checklists=none&members=all&member_fields=avatarHash%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cusername&membersInvited=all&membersInvited_fields=avatarHash%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cusername&memberships_orgMemberType=true&checklists=none&organization=true&organization_fields=name%2CdisplayName%2Cdesc%2CdescData%2Curl%2Cwebsite%2Cprefs%2Cmemberships%2ClogoHash%2Cproducts%2Climits%2CidEnterprise&organization_tags=true&organization_enterprise=true&organization_disable_mock=true&myPrefs=true&fields=name%2Cclosed%2CdateLastActivity%2CdateLastView%2CdatePluginDisable%2CidOrganization%2Cprefs%2CshortLink%2CshortUrl%2Curl%2CcreationMethod%2Cdesc%2CdescData%2CidTags%2Cinvitations%2Cinvited%2ClabelNames%2Climits%2Cmemberships%2CpowerUps%2Csubscribed`
+  const kanban = location.pathname.split('/')[2]
 
-	fetch(url).then(ret => ret.json()).then(json => {
+  const url = `https://trello.com/1/Boards/${kanban}?lists=open&list_fields=name%2Cclosed%2CidBoard%2Cpos%2Csubscribed%2Climits%2CcreationMethod%2CsoftLimit&cards=visible&card_attachments=cover&card_stickers=true&card_fields=badges%2Cclosed%2CdateLastActivity%2Cdesc%2CdescData%2Cdue%2CdueComplete%2CdueReminder%2CidAttachmentCover%2CidList%2CidBoard%2CidMembers%2CidShort%2CidLabels%2Climits%2Cname%2Cpos%2CshortUrl%2CshortLink%2Csubscribed%2Curl%2ClocationName%2Caddress%2Ccoordinates%2Clabels&card_checklists=none&members=all&member_fields=avatarHash%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cusername&membersInvited=all&membersInvited_fields=avatarHash%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cusername&memberships_orgMemberType=true&checklists=none&organization=true&organization_fields=name%2CdisplayName%2Cdesc%2CdescData%2Curl%2Cwebsite%2Cprefs%2Cmemberships%2ClogoHash%2Cproducts%2Climits%2CidEnterprise&organization_tags=true&organization_enterprise=true&organization_disable_mock=true&myPrefs=true&fields=name%2Cclosed%2CdateLastActivity%2CdateLastView%2CdatePluginDisable%2CidOrganization%2Cprefs%2CshortLink%2CshortUrl%2Curl%2CcreationMethod%2Cdesc%2CdescData%2CidTags%2Cinvitations%2Cinvited%2ClabelNames%2Climits%2Cmemberships%2CpowerUps%2Csubscribed`
 
-		const listMap = {}
-		json.lists.forEach(item => {
-			listMap[item.id] = item.name
-		})
+  fetch(url).then(ret => ret.json()).then(json => {
 
-		const whoMap = {}
-		json.members.forEach(item => {
-			whoMap[item.id] = {
-				name: item.initials,
-				cards: [],
-				idListName: ''
-			}
-		})
-		const list = json.cards.forEach((item) => {
-			item.idMembers.forEach(v => {
-				const listName = listMap[item.idList]
-				whoMap[v].cards.push({
-					listName,
-					name: item.name
-				})
-			})
-		})
+    const listMap = {}
+    json.lists.forEach(item => {
+      listMap[item.id] = item.name
+    })
 
-		console.log(whoMap)
+    const whoMap = {}
+    json.members.forEach(item => {
+      whoMap[item.id] = {
+        name: item.initials,
+        cards: [],
+        idListName: ''
+      }
+    })
+    const list = json.cards.forEach((item) => {
+      item.idMembers.forEach(v => {
+        const listName = listMap[item.idList]
 
-		const $board = document.getElementById('board')
-		if (!$board) {
-			return
-		}
+        if (kanban === '6AofN7n8') {
+          if (listName === '待整理排期') {
+            return
+          }
+        }
 
-		for (const key in whoMap) {
-			const item = whoMap[key]
+        whoMap[v].cards.push({
+          listName,
+          name: item.name
+        })
+      })
+    })
 
-			const div = document.createElement('div')
-			div.className = 'js-list list-wrapper'
+    console.log(whoMap)
 
+    const $board = document.getElementById('board')
+    if (!$board) {
+      return
+    }
 
-			let temp = `
+    for (const key in whoMap) {
+      const item = whoMap[key]
+
+      const div = document.createElement('div')
+      div.className = 'js-list list-wrapper'
+
+      let temp = `
 	<div class="list js-list-content">
 		<div class="list-header js-list-header u-clearfix is-menu-shown">
 			<textarea class="list-header-name mod-list-name js-list-name-input" spellcheck="false" dir="auto" maxlength="512" style="overflow: hidden; overflow-wrap: break-word; height: 28px;">
@@ -53,7 +61,7 @@ function showMember() {
 		</div>
 		<div class="list-cards u-fancy-scrollbar u-clearfix js-list-cards js-sortable ui-sortable">
 			${item.cards.map(card => {
-				return `
+        return `
 			<a class="list-card js-member-droppable ui-droppable" href="javascript:;">
 				<div class="list-card-details js-card-details">
 					<span class="list-card-title js-card-name" dir="auto">
@@ -64,34 +72,34 @@ function showMember() {
 				</div>
 			</a>
 				`
-			}).join('')}
+      }).join('')}
 		</div>
 	</div>
 `
 
-			div.innerHTML = temp
+      div.innerHTML = temp
 
-			$board.insertBefore(div, $board.firstChild)
-		}
-	})
+      $board.insertBefore(div, $board.firstChild)
+    }
+  })
 }
 
-function showBtn(){
-	const $headerBtn = document.querySelector('.board-header-btns.mod-right')
+function showBtn () {
+  const $headerBtn = document.querySelector('.board-header-btns.mod-right')
 
-	if(!$headerBtn){
-		return
-	}
+  if (!$headerBtn) {
+    return
+  }
 
-	const a = document.createElement('a')
-	a.className = 'board-header-btn'
-	a.href = 'javascript:;'
-	a.onclick = showMember
-	a.innerHTML = '<span class="board-header-btn-text u-text-underline">按人分</span>'
+  const a = document.createElement('a')
+  a.className = 'board-header-btn'
+  a.href = 'javascript:;'
+  a.onclick = showMember
+  a.innerHTML = '<span class="board-header-btn-text u-text-underline">按人分</span>'
 
-	$headerBtn.insertBefore(a, $headerBtn.firstChild)
+  $headerBtn.insertBefore(a, $headerBtn.firstChild)
 }
 
 setTimeout(() => {
-	showBtn()
+  showBtn()
 }, 2000)
